3d24e4dad166d5c164e5afacd83f3d266ff9296a
pits <pits@debian>
2024-09-24 16:36:16 +0200

    measure 100, 1000 and 100000 cases

---
+++ TD1/measuring_runtime.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <iostream>
+
+int sum_array(int array[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += array[i];
+    }
+    return sum;
+}
+
+void measure(int nb_elements){
+    std::cout << "Number of elements : " << nb_elements<< std::endl;
+
+    auto init_start_time_point = std::chrono::high_resolution_clock::now();
+
+    int even_integers[nb_elements];
+
+    for (int i = 0; i < nb_elements; ++i) {
+        even_integers[i] = 2*i;
+    }
+
+    auto init_end_time_point = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Initialization time: " << (init_end_time_point-init_start_time_point).count() << std::endl;
+
+    init_start_time_point = std::chrono::high_resolution_clock::now();
+
+    int sum = sum_array(even_integers, nb_elements);
+
+    init_end_time_point = std::chrono::high_resolution_clock::now();
+
+    std::cout << "Sum time : " << (init_end_time_point-init_start_time_point).count() << std::endl;
+    std::cout << "Result : " << sum << std::endl;
+    std::cout << "---------------------------" << std::endl;
+}
+
+int main() {
+    measure(1000);
+    measure(10000);
+    measure(100000);
+    return 0;
+}
