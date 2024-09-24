964837bfbacc0f9d646dc01c4ee778e2251aef4c
pits <pits@debian>
2024-09-24 15:43:45 +0200

    with a function

--- TD1/main.cpp
+++ TD1/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
-int main(void) {
-    for (int i=1; i <= 5; ++i) {
+void printGreetings(int n) {
+    for (int i=1; i <= n/2; ++i) {
         std::cout << 2*i << std::endl;
     }
 
     std::cout << "Hello World !" << std::endl;
 
-    for (int i=4; i >= 0; --i) {
-        std::cout << 2*i << std::endl;
+    for (int i=(n-1)/2; i >= 0; --i) {
+        std::cout << 2*i + 1 << std::endl;
     }
+}
+
+int main(void) {
+    printGreetings(9);
+    printGreetings(10);
+    return 0;
 }
